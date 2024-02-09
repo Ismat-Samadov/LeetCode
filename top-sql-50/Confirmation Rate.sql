7af22476d54cf3a0267e21c3a49eec2635025268
Ismat <ismetsemedov@gmail.com>
2024-02-09 09:59:27 +0400

    Update Confirmation Rate.sql

--- top-sql-50/Confirmation Rate.sql
+++ top-sql-50/Confirmation Rate.sql
@@ -76,6 +76,15 @@
 -- User 7 made 3 requests and all were confirmed. The confirmation rate is 1.
 -- User 2 made 2 requests where one was confirmed and the other timed out. The confirmation rate is 1 / 2 = 0.5.
 
+SELECT 
+    s.user_id,
+    ROUND(NVL(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / NULLIF(COUNT(c.user_id), 0), 0), 2) AS confirmation_rate
+FROM 
+    Signups s
+LEFT JOIN 
+    Confirmations c ON s.user_id = c.user_id
+GROUP BY 
+    s.user_id;
 
 
 
