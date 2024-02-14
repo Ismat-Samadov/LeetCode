7eb0e8b75504c522f72e5bc7a322c95d91557d0d
Ismat <ismetsemedov@gmail.com>
2024-02-14 20:10:47 +0400

    Update Average Selling Price.sql

--- top-sql-50/Average Selling Price.sql
+++ top-sql-50/Average Selling Price.sql
@@ -66,3 +66,8 @@
 -- Average selling price = Total Price of Product / Number of products sold.
 -- Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
 -- Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
+select a.product_id,
+ COALESCE(ROUND(SUM(a.price * COALESCE(b.units, 0)) / NULLIF(SUM(b.units), 0), 2), 0) AS average_price 
+from Prices a left join UnitsSold b on a.product_id = b.product_id 
+and b.purchase_date BETWEEN a.start_date AND a.end_date
+group by a.product_id; 
