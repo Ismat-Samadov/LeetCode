cc58dcb16c7622b5301f110aeb726de81fde2940
Ismat <ismetsemedov@gmail.com>
2024-02-15 15:12:20 +0400

    Update Project Employees I.sql

--- top-sql-50/Project Employees I.sql
+++ top-sql-50/Project Employees I.sql
@@ -62,3 +62,7 @@
 -- | 2           | 2.50          |
 -- +-------------+---------------+
 -- Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+select  
+a.project_id ,avg(b.experience_years) average_years
+from project a inner join employee b on a.employee_id=b.employee_id
+group by a.project_id;
