f81799bea11193a0db73738df7de6c2521dc7c36
Ismat <ismetsemedov@gmail.com>
2024-02-16 15:02:03 +0400

    Update Percentage of Users Attended a Contest.sql

--- top-sql-50/Percentage of Users Attended a Contest.sql
+++ top-sql-50/Percentage of Users Attended a Contest.sql
@@ -72,3 +72,6 @@
 -- All the users registered in contests 208, 209, and 210. The percentage is 100% and we sort them in the answer table by contest_id in ascending order.
 -- Alice and Alex registered in contest 215 and the percentage is ((2/3) * 100) = 66.67%
 -- Bob registered in contest 207 and the percentage is ((1/3) * 100) = 33.33%
+select b.contest_id,round(count( a.user_id)/(select count(*) from Users)*100,2) as percentage 
+from Users a inner join Register b on a.user_id=b.user_id group by b.contest_id order by percentage desc,b.contest_id asc
+
